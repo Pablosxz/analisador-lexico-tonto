d71d550880843c4b2e88a063c8f0480d4f8074d4
Clara Letícia <claralet1346@gmail.com>
2026-09-13 18:38:34 -0300

    feat(especificacao): definindo as palavras reconhecidas da linguagem

---
+++ src/especificacao.py
@@ -0,0 +1,48 @@
+# Vocabulario da linguagem TONTO.
+# Acrescentar ou remover uma palavra se resolve aqui, sem mexer nas expressoes
+# regulares: o lexer.py consulta RESERVADAS depois de casar o lexema.
+
+# Estereotipos de classe
+_EST_CLASSE = """
+    event situation process category mixin phaseMixin roleMixin historicalRoleMixin
+    kind collective quantity quality mode intrinsicMode extrinsicMode subkind phase
+    role historicalRole relator
+""".split()
+
+# Estereotipos de relacao
+_EST_RELACAO = """
+    material derivation comparative mediation characterization externalDependence
+    componentOf memberOf subCollectionOf subQualityOf instantiation termination
+    participational participation historicalDependence creation manifestation
+    bringsAbout triggers composition aggregation inherence value formal constitution
+""".split()
+
+# Palavras reservadas
+_RESERVADA = """
+    genset disjoint complete general specifics where package import
+    functional-complexes specializes of relation enum relators intrinsic-modes extrinsic-modes
+""".split()
+
+# Tipos de dado nativos
+_TIPO_NATIVO = "number string boolean date time datetime".split()
+
+# Meta-atributos
+_META_ATRIBUTO = "ordered const derived subsets redefines".split()
+
+# Lexema -> nome do token. Em lexer.py:65 o default e 'NOME_RELACAO': se a
+# palavra nao esta aqui, e um nome escrito pelo usuario.
+
+RESERVADAS = {}
+
+for _palavra in _EST_CLASSE:
+    RESERVADAS[_palavra] = 'EST_CLASSE'
+for _palavra in _EST_RELACAO:
+    RESERVADAS[_palavra] = 'EST_RELACAO'
+for _palavra in _RESERVADA:
+    RESERVADAS[_palavra] = 'RESERVADA'
+for _palavra in _TIPO_NATIVO:
+    RESERVADAS[_palavra] = 'TIPO_NATIVO'
+for _palavra in _META_ATRIBUTO:
+    RESERVADAS[_palavra] = 'META_ATRIBUTO'
+
+
